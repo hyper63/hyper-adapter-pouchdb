d0315cb24f0fa4495d8d4fefb39cc8f54e6edae9
Tyler Hall <tyler@hyper.io>
2022-04-05 15:12:03 +0000

    feat: add utils and api for managing pouchdbs

--- dev_deps.js
+++ dev_deps.js
@@ -3,4 +3,7 @@ export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.1.
 export { data as validateDataAdapterSchema } from "https://x.nest.land/hyper-port-data@1.2.0/mod.js";
 
 // std lib deps
-export { assert } from "https://deno.land/std@0.132.0/testing/asserts.ts";
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.133.0/testing/asserts.ts";
