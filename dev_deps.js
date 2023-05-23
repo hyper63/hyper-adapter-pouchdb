9d737b5c2f24e26b08dc7aca65f2a53d0c169da0
Tyler Hall <tyler@hyper.io>
2023-05-23 19:21:20 -0400

    refactor: remove nest.land imports

--- dev_deps.js
+++ dev_deps.js
@@ -1,7 +1,3 @@
-// Schema parsing deps
-export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.3.0/utils/plugin-schema.js";
-export { data as validateDataAdapterSchema } from "https://x.nest.land/hyper-port-data@1.2.1/mod.js";
-
 // std lib deps
 export {
   assert,
