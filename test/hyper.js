cfff7e5df13aca7c551dd71c4c90741ecb8f39ad
Tyler Hall <tyler@hyper.io>
2023-11-28 19:07:59 -0500

    test: bump harness deps

--- test/hyper.js
+++ test/hyper.js
@@ -1,5 +1,5 @@
-import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.1.0/packages/core/mod.ts'
-import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.1.0/packages/app-express/mod.ts'
+import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.1/packages/core/mod.ts'
+import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.1/packages/app-express/mod.ts'
 
 import myAdapter from '../mod.js'
 import PORT_NAME from '../port_name.js'
